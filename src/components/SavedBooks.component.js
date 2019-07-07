4e92d5523938601ad7797287cafa326ce8d8e5f9
EthanB <ebonsignori@gmail.com>
2019-07-07 14:15:47 -0400

    navbar, logo, empty components for pages

---
+++ src/components/SavedBooks.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SavedBooks extends Component {
+  render () {
+    return (
+      <div>
+        <h2>This is the SavedBooks Component.</h2>
+      </div>
+    )
+  }
+}
+
+export default SavedBooks
---
+++ src/components/SearchBooks.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchBooks extends Component {
+  render () {
+    return (
+      <div>
+        <h2>This is the SearchBooks Component.</h2>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks
