933c20c4f422c817f76fd9be4877b13ff0349ce0
Ethan <ebonsignori@gmail.com>
2019-07-10 03:43:18 -0400

    change import in SearchBooks.component.js

--- client/src/pages/SearchBooks.component.js
+++ client/src/pages/SearchBooks.component.js
@@ -3,7 +3,7 @@ import _ from 'lodash'
 import { Container, Card, Form, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faBook, faBookOpen } from '@fortawesome/free-solid-svg-icons'
-import searchGoogleBooks from '../utils/api'
+import { searchGoogleBooks } from '../utils/api'
 import BookCard from '../components/BookCard.component'
 
 class SearchBooks extends Component {
