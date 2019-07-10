ce719ec29b1ddf460257651461f17f18ac077e82
Ethan <ebonsignori@gmail.com>
2019-07-10 04:13:27 -0400

    refactor routes for heroku

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 const searchGoogleBooks = async (query) => {
-  const url = `http://localhost:4000/api/${query}`
+  const url = `/api/${query}`
   const fetchRes = await window.fetch(url)
   const content = await fetchRes.json()
   return content
--- client/src/utils/db.js
+++ client/src/utils/db.js
@@ -1,12 +1,12 @@
 const db = {
   getBooks: async () => {
-    const fetchRes = await window.fetch('http://localhost:4000/books', { method: 'GET' })
+    const fetchRes = await window.fetch('/books', { method: 'GET' })
     const content = await fetchRes.json()
     return content
   },
 
   saveBook: async (book) => {
-    const fetchRes = await window.fetch('http://localhost:4000/books', {
+    const fetchRes = await window.fetch('/books', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -19,7 +19,7 @@ const db = {
   },
 
   unsaveBook: async (id) => {
-    const fetchRes = await window.fetch(`http://localhost:4000/books/${id}`, { method: 'DELETE' })
+    const fetchRes = await window.fetch(`/books/${id}`, { method: 'DELETE' })
     const status = await fetchRes.json()
     console.log(status)
     return status
