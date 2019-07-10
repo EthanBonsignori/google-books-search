fb65be3939dff7249494e2ea75d737ee3dffbedb
Ethan <ebonsignori@gmail.com>
2019-07-10 14:06:38 -0400

    log url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,6 +3,7 @@ const searchGoogleBooks = async (query) => {
   console.log(process.env.NODE_ENV)
   let url = `/api/${query}`
   if (process.env.NODE_ENV === 'development') url = `http://localhost:4000/api/${query}`
+  console.log(url)
   const fetchRes = await window.fetch(url)
   const content = await fetchRes.json()
   console.log('Content from searchGoogleBooks:')
