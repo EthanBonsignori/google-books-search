5a8a7b3f8f6ef38b3809f7f32d2d7a9b7f3308e6
Ethan <ebonsignori@gmail.com>
2019-07-10 03:55:52 -0400

    change export in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,6 @@
-export default {
-  searchGoogleBooks: async (query) => {
-    const url = `http://localhost:4000/api/${query}`
-    const fetchRes = await window.fetch(url)
-    const content = await fetchRes.json()
-    return content
-  }
+export const searchGoogleBooks = async (query) => {
+  const url = `http://localhost:4000/api/${query}`
+  const fetchRes = await window.fetch(url)
+  const content = await fetchRes.json()
+  return content
 }
