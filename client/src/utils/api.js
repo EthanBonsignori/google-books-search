733019c0fb364142ac56070050b485dad30b8146
Ethan <ebonsignori@gmail.com>
2019-07-10 14:10:46 -0400

    remove duplicate file

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ const searchGoogleBooks = async (query) => {
   let url = `/api/${query}`
   if (process.env.NODE_ENV === 'development') url = `http://localhost:4000/api/${query}`
   console.log(url)
-  const fetchRes = await window.fetch(url)
+  const fetchRes = await window.fetch(url, { method: 'GET' })
   const content = await fetchRes.json()
   console.log('Content from searchGoogleBooks:')
   console.log(content)
