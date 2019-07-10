3d6f1cc490b13ba708ca5819912c03b50b20f4c8
Ethan <ebonsignori@gmail.com>
2019-07-09 23:12:31 -0400

    pre-refactor

---
+++ server/models/Book.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BookSchema = new Schema({
+  authors: {
+    type: Array
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  link: {
+    type: String,
+    unique: true
+  },
+  title: {
+    type: String
+  }
+})
+
+const Book = mongoose.model('Book', BookSchema)
+
+module.exports = Book
