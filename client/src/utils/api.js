b6bd13fb38dc7ce440452f3a24afe360084986af
Ethan <ebonsignori@gmail.com>
2019-07-10 11:50:04 -0400

    rm comments, add checker to api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,8 @@
 const searchGoogleBooks = async (query) => {
   console.log('In searchGoogleBooks')
-  const url = `http://localhost:4000/api/${query}`
+  console.log(process.env.NODE_ENV)
+  let url = `/api/${query}`
+  if (process.env.NODE_ENV === 'development') url = `http://localhost:4000/api/${query}`
   const fetchRes = await window.fetch(url)
   const content = await fetchRes.json()
   console.log('Content from searchGoogleBooks:')
